f17790b2a6223734029e95fdf579c3380069357b
Ismo Vuorinen <ismo@ivuorinen.net>
2026-01-25 10:19:27 +0200

    refactor: consolidate logrus adapter with embedded loggerCore
    Introduce loggerCore type that provides the 8 standard logging methods
    (Debug, Info, Warn, Error, Debugf, Infof, Warnf, Errorf). Both
    logrusAdapter and logrusEntryAdapter now embed this type, eliminating
    16 duplicate method implementations.

--- fail2ban/logrus_adapter.go
+++ fail2ban/logrus_adapter.go
@@ -2,14 +2,47 @@ package fail2ban
 
 import "github.com/sirupsen/logrus"
 
-// logrusAdapter wraps logrus to implement our decoupled LoggerInterface
-type logrusAdapter struct {
+// loggerCore provides common logging methods that delegate to a logrus.Entry.
+// This type is embedded in both logrusAdapter and logrusEntryAdapter to
+// eliminate duplicate method implementations.
+type loggerCore struct {
 	entry *logrus.Entry
 }
 
-// logrusEntryAdapter wraps logrus.Entry to implement LoggerEntry
+// Debug logs a debug-level message.
+func (c *loggerCore) Debug(args ...interface{}) { c.entry.Debug(args...) }
+
+// Info logs an info-level message.
+func (c *loggerCore) Info(args ...interface{}) { c.entry.Info(args...) }
+
+// Warn logs a warning-level message.
+func (c *loggerCore) Warn(args ...interface{}) { c.entry.Warn(args...) }
+
+// Error logs an error-level message.
+func (c *loggerCore) Error(args ...interface{}) { c.entry.Error(args...) }
+
+// Debugf logs a formatted debug-level message.
+func (c *loggerCore) Debugf(format string, args ...interface{}) { c.entry.Debugf(format, args...) }
+
+// Infof logs a formatted info-level message.
+func (c *loggerCore) Infof(format string, args ...interface{}) { c.entry.Infof(format, args...) }
+
+// Warnf logs a formatted warning-level message.
+func (c *loggerCore) Warnf(format string, args ...interface{}) { c.entry.Warnf(format, args...) }
+
+// Errorf logs a formatted error-level message.
+func (c *loggerCore) Errorf(format string, args ...interface{}) { c.entry.Errorf(format, args...) }
+
+// logrusAdapter wraps logrus to implement our decoupled LoggerInterface.
+// It embeds loggerCore to provide the 8 standard logging methods.
+type logrusAdapter struct {
+	loggerCore // embeds Debug, Info, Warn, Error, Debugf, Infof, Warnf, Errorf
+}
+
+// logrusEntryAdapter wraps logrus.Entry to implement LoggerEntry.
+// It embeds loggerCore to provide the 8 standard logging methods.
 type logrusEntryAdapter struct {
-	entry *logrus.Entry
+	loggerCore // embeds Debug, Info, Warn, Error, Debugf, Infof, Warnf, Errorf
 }
 
 // Ensure logrusAdapter implements LoggerInterface
@@ -23,117 +56,37 @@ func NewLogrusAdapter(logger *logrus.Logger) LoggerInterface {
 	if logger == nil {
 		logger = logrus.StandardLogger()
 	}
-	return &logrusAdapter{entry: logrus.NewEntry(logger)}
+	return &logrusAdapter{loggerCore: loggerCore{entry: logrus.NewEntry(logger)}}
 }
 
 // WithField implements LoggerInterface
 func (l *logrusAdapter) WithField(key string, value interface{}) LoggerEntry {
-	return &logrusEntryAdapter{entry: l.entry.WithField(key, value)}
+	return &logrusEntryAdapter{loggerCore: loggerCore{entry: l.entry.WithField(key, value)}}
 }
 
 // WithFields implements LoggerInterface
 func (l *logrusAdapter) WithFields(fields Fields) LoggerEntry {
-	return &logrusEntryAdapter{entry: l.entry.WithFields(logrus.Fields(fields))}
+	return &logrusEntryAdapter{loggerCore: loggerCore{entry: l.entry.WithFields(logrus.Fields(fields))}}
 }
 
 // WithError implements LoggerInterface
 func (l *logrusAdapter) WithError(err error) LoggerEntry {
-	return &logrusEntryAdapter{entry: l.entry.WithError(err)}
-}
-
-// Debug implements LoggerInterface
-func (l *logrusAdapter) Debug(args ...interface{}) {
-	l.entry.Debug(args...)
-}
-
-// Info implements LoggerInterface
-func (l *logrusAdapter) Info(args ...interface{}) {
-	l.entry.Info(args...)
-}
-
-// Warn implements LoggerInterface
-func (l *logrusAdapter) Warn(args ...interface{}) {
-	l.entry.Warn(args...)
-}
-
-// Error implements LoggerInterface
-func (l *logrusAdapter) Error(args ...interface{}) {
-	l.entry.Error(args...)
-}
-
-// Debugf implements LoggerInterface
-func (l *logrusAdapter) Debugf(format string, args ...interface{}) {
-	l.entry.Debugf(format, args...)
-}
-
-// Infof implements LoggerInterface
-func (l *logrusAdapter) Infof(format string, args ...interface{}) {
-	l.entry.Infof(format, args...)
-}
-
-// Warnf implements LoggerInterface
-func (l *logrusAdapter) Warnf(format string, args ...interface{}) {
-	l.entry.Warnf(format, args...)
-}
-
-// Errorf implements LoggerInterface
-func (l *logrusAdapter) Errorf(format string, args ...interface{}) {
-	l.entry.Errorf(format, args...)
+	return &logrusEntryAdapter{loggerCore: loggerCore{entry: l.entry.WithError(err)}}
 }
 
 // LoggerEntry implementation for logrusEntryAdapter
 
 // WithField implements LoggerEntry
 func (e *logrusEntryAdapter) WithField(key string, value interface{}) LoggerEntry {
-	return &logrusEntryAdapter{entry: e.entry.WithField(key, value)}
+	return &logrusEntryAdapter{loggerCore: loggerCore{entry: e.entry.WithField(key, value)}}
 }
 
 // WithFields implements LoggerEntry
 func (e *logrusEntryAdapter) WithFields(fields Fields) LoggerEntry {
-	return &logrusEntryAdapter{entry: e.entry.WithFields(logrus.Fields(fields))}
+	return &logrusEntryAdapter{loggerCore: loggerCore{entry: e.entry.WithFields(logrus.Fields(fields))}}
 }
 
 // WithError implements LoggerEntry
 func (e *logrusEntryAdapter) WithError(err error) LoggerEntry {
-	return &logrusEntryAdapter{entry: e.entry.WithError(err)}
-}
-
-// Debug implements LoggerEntry
-func (e *logrusEntryAdapter) Debug(args ...interface{}) {
-	e.entry.Debug(args...)
-}
-
-// Info implements LoggerEntry
-func (e *logrusEntryAdapter) Info(args ...interface{}) {
-	e.entry.Info(args...)
-}
-
-// Warn implements LoggerEntry
-func (e *logrusEntryAdapter) Warn(args ...interface{}) {
-	e.entry.Warn(args...)
-}
-
-// Error implements LoggerEntry
-func (e *logrusEntryAdapter) Error(args ...interface{}) {
-	e.entry.Error(args...)
-}
-
-// Debugf implements LoggerEntry
-func (e *logrusEntryAdapter) Debugf(format string, args ...interface{}) {
-	e.entry.Debugf(format, args...)
-}
-
-// Infof implements LoggerEntry
-func (e *logrusEntryAdapter) Infof(format string, args ...interface{}) {
-	e.entry.Infof(format, args...)
-}
-
-// Warnf implements LoggerEntry
-func (e *logrusEntryAdapter) Warnf(format string, args ...interface{}) {
-	e.entry.Warnf(format, args...)
-}
-
-// Errorf implements LoggerEntry
-func (e *logrusEntryAdapter) Errorf(format string, args ...interface{}) {
-	e.entry.Errorf(format, args...)
+	return &logrusEntryAdapter{loggerCore: loggerCore{entry: e.entry.WithError(err)}}
 }
